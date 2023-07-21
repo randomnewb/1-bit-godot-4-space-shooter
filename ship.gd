bc6adb9aae75e10dcce7a72de8499565a410ff3d
randomnewb <nguy1581@gmail.com>
2023-07-21 12:09:07 -0500

    added enemy spawner; player, enemy, and bullet can be removed,

--- ship.gd
+++ ship.gd
@@ -12,5 +12,9 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		var laser = LASER_SCENE.instantiate();
 		var world = get_tree().current_scene;
-		world.add_child(laser)
-		laser.position = position
+		world.add_child(laser);
+		laser.position = position;
+
+func _on_area_entered(area):
+	queue_free();
+	area.queue_free();
