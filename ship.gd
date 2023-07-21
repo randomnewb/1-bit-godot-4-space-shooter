b979e8d4854e6c70451dd87684691684de11e836
randomnewb <nguy1581@gmail.com>
2023-07-21 11:44:24 -0500

    added a laser scene, ship can fire laser
    The laser scene is first preloaded, instantiated, added to the world node (the root of the current scene), added as a child of the world, and to where the position of the ship is

--- ship.gd
+++ ship.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
+const LASER_SCENE = preload("res://laser.tscn");
+
 @export var speed = 100;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta;
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta;
+	if Input.is_action_just_pressed("ui_accept"):
+		var laser = LASER_SCENE.instantiate();
+		var world = get_tree().current_scene;
+		world.add_child(laser)
+		laser.position = position
